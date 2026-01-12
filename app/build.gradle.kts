d375353505d991667aa8242edc6963e72529ced8
Tobias Domáň <domantobias@gmail.com>
2026-01-12 01:42:33 +0100

    chore: build, config, dependency updates to newer versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,15 +41,17 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
+
     buildFeatures {
         compose = true
         buildConfig = true
+    }
+
+    kotlin {
+        jvmToolchain(21)
     }
 }
 
@@ -62,6 +64,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -73,7 +76,7 @@ dependencies {
     implementation("com.google.android.material:material:1.13.0")
 
     // supabase
-    implementation(platform("io.github.jan-tennert.supabase:bom:3.2.6"))
+    implementation(platform("io.github.jan-tennert.supabase:bom:3.3.0"))
     implementation("io.github.jan-tennert.supabase:postgrest-kt")
     implementation("io.github.jan-tennert.supabase:auth-kt")
     implementation("io.github.jan-tennert.supabase:realtime-kt")
@@ -81,5 +84,11 @@ dependencies {
     implementation("io.ktor:ktor-client-android:3.3.3")
 
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.10.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.10.0")
+
+    implementation("io.coil-kt:coil-compose:2.7.0")
+    implementation("io.coil-kt:coil-svg:2.7.0")
+    implementation("androidx.compose.material:material-icons-extended:1.7.8")
+
+    implementation("androidx.datastore:datastore-preferences:1.2.0")
 }
